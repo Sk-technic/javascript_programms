9a71a96f4d360210f5927d7b41a20d2ef47e6430
Sk-technic <ssk113054@gmail.com>
2025-01-22 12:24:27 +0530

    callbacks & recursion

---
+++ javascript/callback.js
@@ -0,0 +1,29 @@
+//----------recursion---------//
+//the function call it self is call recursion..
+console.log("hhh");
+
+function factorial(num) {
+  if (num>1) {
+    return num * factorial(num - 1)
+  }
+  else{
+    return 1
+  }
+}
+let x = 3
+let y = factorial(x)
+
+// console.log(`the factorial of ${x} is ${y}`);
+
+//--------Callbacks----------------//
+
+function greet(name, callback) {
+    console.log(`Hello, ${name}!`);
+    callback();
+}
+
+function sayGoodbye() {
+    console.log("Goodmorning!");
+}
+
+greet("shubahm", sayGoodbye);
