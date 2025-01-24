717659c765935dfeb90452ac3be34ae6e6c402a5
Sk-technic <ssk113054@gmail.com>
2025-01-24 12:58:24 +0530

    ajax

--- javascript/callback.js
+++ javascript/callback.js
@@ -1,29 +1,43 @@
 //----------recursion---------//
 //the function call it self is call recursion..
-console.log("hhh");
+// console.log("hhh");
 
-function factorial(num) {
-  if (num>1) {
-    return num * factorial(num - 1)
-  }
-  else{
-    return 1
-  }
-}
-let x = 3
-let y = factorial(x)
+// function factorial(num) {
+//   if (num>1) {
+//     return num * factorial(num - 1)
+//   }
+//   else{
+//     return 1
+//   }
+// }
+// let x = 3
+// let y = factorial(x)
 
 // console.log(`the factorial of ${x} is ${y}`);
 
 //--------Callbacks----------------//
 
-function greet(name, callback) {
-    console.log(`Hello, ${name}!`);
-    callback();
-}
+// function greet(name, callback) {
+//     console.log(`Hello, ${name}!`);
+//     callback();
+// }
 
-function sayGoodbye() {
-    console.log("Goodmorning!");
-}
+// function sayGoodbye() {
+//     console.log("Goodmorning!");
+// }
 
-greet("shubahm", sayGoodbye);
+// greet("shubahm", sayGoodbye);
+
+// function hlo(name,callback) {
+//   callback();
+//   console.log(`hello, ${name}`);
+
+  
+// }
+
+// function user() {
+//   console.log("shubham","goodmorning");
+  
+// }
+
+// hlo("haspreet",user);
